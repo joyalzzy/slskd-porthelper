0eadbf1e92e7ee3619bc33b013eb0e79c646b632
joyalzzy <joyalzzy@homebitch.homebitch.joyalzzy>
2023-06-27 12:51:51 +0800

    fixed sed

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
-FROM bash:4.4
+FROM alpine
 # COPY pia-shared /pia-shared
+RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing/" >> /etc/apk/repositories && \
+    echo "http://dl-cdn.alpinelinux.org/alpine/edge/community/" >> /etc/apk/repositories && \
+    echo "http://dl-cdn.alpinelinux.org/alpine/edge/main/" >> /etc/apk/repositories && \
+    apk add --no-cache bash inotify-tools
+
 COPY port.sh /port.sh
 # COPY slskd.yml /slskd.yml
-CMD ["bash", "port.sh"]
+RUN chmod +x /port.sh
+
+CMD ["/port.sh"]
--- port.sh
+++ port.sh
@@ -1,2 +1,23 @@
-#!/bin/sh
-sed 's/^\(listenPort: \).*$/\1'"$(cat /pia-shared/port.dat)"'/' /slskd.yml
+#!/bin/bash
+
+trap 'exit 0' SIGTERM
+
+OLDPORT=0
+PORT=0
+
+while true
+do
+	[ -r "/pia-shared/port.dat" ] && PORT=$(cat /pia-shared/port.dat)
+
+	if [ $OLDPORT -ne $PORT ]; then
+		echo "Setting Soulseek port to ($PORT)."	
+#		cat /slskd.yml
+		
+		rc=$(sed 's/listen_port:.*$/listen_port: '$PORT'/' /slskd.yml)
+		echo "$rc" > /slskd.yml
+		OLDPORT=$PORT
+	fi
+
+	inotifywait --event modify --event create /pia-shared
+done
+
